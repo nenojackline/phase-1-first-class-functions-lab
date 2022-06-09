97556f5c42c087c0b25c07ae07275479dc89e47e
jackline chepngeno <chepngenojackline0@gmail.com>
2022-06-09 07:15:06 +0300

    commit tree 4d3e4975b43c5bc7a2fd3b5d7593cca8b3e7e47b

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const drivers = ['Lewis', 'John', 'Joe', 'Doe'];
+
+const returnFirstTwoDrivers = function(array) {
+    const firstTwoDrivers = array.slice(0, 2);
+    return firstTwoDrivers;
+}
+const returnLastTwoDrivers = function(array) {
+    const lastTwoDrivers = array.slice(2, 4);
+    return lastTwoDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(price) {
+    return function() {
+        const fare = price * price;
+        return fare;
+    }
+}
+
+function fareDoubler(fare) {
+    createFareMultiplier(fare);
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    createFareMultiplier(fare);
+    return fare * 3;
+}
+
+function selectDifferentDrivers(array, driversFunction) {
+    if (driversFunction === returnFirstTwoDrivers) {
+        returnFirstTwoDrivers(array);
+    } else if (driversFunction === returnLastTwoDrivers) {
+        returnLastTwoDrivers(array);
+    }
+    return driversFunction(array);
+}
